2898e3cbf9acd1b31c016caed331a33a23fbe4db
Laura Caroline <laura@laura-melo.com>
2024-06-01 22:51:27 +0200

    feat: add tooltip description to tools

--- src/main/java/com/hanawa/animalfinder/item/custom/AnimalFinderToolItem.java
+++ src/main/java/com/hanawa/animalfinder/item/custom/AnimalFinderToolItem.java
@@ -13,10 +13,13 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.*;
 
@@ -30,6 +33,29 @@ public class AnimalFinderToolItem extends Item {
         this.distance = distance;
         this.maxSlots = maxSlots;
         this.indexedEntities = new ArrayList<>();
+    }
+
+    @Override
+    public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> tooltipComnponents, TooltipFlag isAdvanced) {
+        String lastComponent = "";
+        String firstComponents = "";
+
+        if (indexedEntities.size() > 0) {
+            lastComponent = I18n.get(indexedEntities.get(indexedEntities.size() - 1));
+        }
+
+        if (indexedEntities.size() > 1) {
+            firstComponents = getTranslatedEntitiesName(indexedEntities.subList(0, indexedEntities.size() - 1));
+        }
+
+        tooltipComnponents.add(
+            Component.translatable(
+            "animalfinder.tool.tooltip",
+                firstComponents,
+                lastComponent
+            )
+        );
+        super.appendHoverText(stack, level, tooltipComnponents, isAdvanced);
     }
 
     @Override
