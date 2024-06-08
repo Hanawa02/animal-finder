6527690a3a1de5209fed779b612a30482651c2e2
Laura Caroline <laura@laura-melo.com>
2024-06-08 02:11:45 +0200

    fix: adjust range based on tier

--- src/main/java/com/hanawa/animalfinder/item/AnimalFinderModItems.java
+++ src/main/java/com/hanawa/animalfinder/item/AnimalFinderModItems.java
@@ -14,8 +14,8 @@ public class AnimalFinderModItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, AnimalFinder.MOD_ID);
 
     public static final RegistryObject<Item> BASIC_ANIMAL_FINDER = ITEMS.register("basic_animal_finder", () -> new AnimalFinderToolItem(new Item.Properties(), 32, 1));
-    public static final RegistryObject<Item> COPPER_ANIMAL_FINDER = ITEMS.register("copper_animal_finder", () -> new AnimalFinderToolItem(new Item.Properties(), 48, 2));
-    public static final RegistryObject<Item> IRON_ANIMAL_FINDER = ITEMS.register("iron_animal_finder", () -> new AnimalFinderToolItem(new Item.Properties(), 64, 4));
+    public static final RegistryObject<Item> COPPER_ANIMAL_FINDER = ITEMS.register("copper_animal_finder", () -> new AnimalFinderToolItem(new Item.Properties(), 64, 2));
+    public static final RegistryObject<Item> IRON_ANIMAL_FINDER = ITEMS.register("iron_animal_finder", () -> new AnimalFinderToolItem(new Item.Properties(), 96, 4));
     public static final RegistryObject<Item> GOLD_ANIMAL_FINDER = ITEMS.register("gold_animal_finder", () -> new AnimalFinderToolItem(new Item.Properties(), 128, 8));
     public static final RegistryObject<Item> DIAMOND_ANIMAL_FINDER = ITEMS.register("diamond_animal_finder", () -> new AnimalFinderToolItem(new Item.Properties(), 256, 256));
 
