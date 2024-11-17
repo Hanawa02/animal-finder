14a1302b9488028fdecfbafcd52ecceeab9c1582
Laura Caroline <laura@laura-melo.com>
2024-11-17 18:41:14 +0100

    chore: port to version 1.20.1

--- src/main/java/com/hanawa/animalfinder/datagen/ModRecipeProvider.java
+++ src/main/java/com/hanawa/animalfinder/datagen/ModRecipeProvider.java
@@ -3,8 +3,9 @@ package com.hanawa.animalfinder.datagen;
 
 import com.hanawa.animalfinder.item.ModItems;
 import net.minecraft.data.PackOutput;
+import net.minecraft.data.recipes.FinishedRecipe;
 import net.minecraft.data.recipes.RecipeCategory;
-import net.minecraft.data.recipes.RecipeOutput;
+import net.minecraft.data.recipes.FinishedRecipe;
 import net.minecraft.data.recipes.RecipeProvider;
 import net.minecraft.data.recipes.ShapedRecipeBuilder;
 import net.minecraft.tags.ItemTags;
@@ -14,6 +15,8 @@ import net.minecraft.world.item.Items;
 import net.minecraftforge.common.Tags;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.function.Consumer;
+
 public class ModRecipeProvider extends RecipeProvider {
     public ModRecipeProvider(PackOutput pOutput) {
         super(pOutput);
@@ -21,7 +24,7 @@ public class ModRecipeProvider extends RecipeProvider {
 
 
     @Override
-    protected void buildRecipes(@NotNull RecipeOutput recipeOutput) {
+    protected void buildRecipes(@NotNull Consumer<FinishedRecipe> recipeOutput) {
         finderToolRecipe(recipeOutput, ModItems.BASIC_ANIMAL_FINDER.get(), Items.COMPASS, ItemTags.PLANKS, Tags.Items.INGOTS_COPPER);
         finderToolRecipe(recipeOutput, ModItems.COPPER_ANIMAL_FINDER.get(), ModItems.BASIC_ANIMAL_FINDER.get(), Tags.Items.INGOTS_COPPER, Tags.Items.INGOTS_IRON);
         finderToolRecipe(recipeOutput, ModItems.IRON_ANIMAL_FINDER.get(), ModItems.COPPER_ANIMAL_FINDER.get(), Tags.Items.INGOTS_IRON, Tags.Items.INGOTS_GOLD);
@@ -30,7 +33,7 @@ public class ModRecipeProvider extends RecipeProvider {
     }
 
 
-    protected static void finderToolRecipe(RecipeOutput recipeOutput, Item recipeItem, Item core, TagKey<Item> frame, TagKey<Item> connector ) {
+    protected static void finderToolRecipe(Consumer<FinishedRecipe> recipeOutput, Item recipeItem, Item core, TagKey<Item> frame, TagKey<Item> connector ) {
         ShapedRecipeBuilder
                 .shaped(RecipeCategory.MISC, recipeItem, 1)
                 .define('A', frame)
